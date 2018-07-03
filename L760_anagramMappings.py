07bb69a989f6c5f8b91cfc8e441b8bd7e61039df
Chunji Wang <chunjiw@users.noreply.github.com>
2018-07-02 23:21:41 -0700

    Create L760_anagramMappings.py

---
+++ L760_anagramMappings.py
@@ -0,0 +1,37 @@
+# 760. Find Anagram Mappings
+# DescriptionHintsSubmissionsDiscussSolution
+# Given two lists Aand B, and B is an anagram of A. B is an anagram of A means B is made by randomizing the order of the elements in A.
+
+# We want to find an index mapping P, from A to B. A mapping P[i] = j means the ith element in A appears in B at index j.
+
+# These lists A and B may contain duplicates. If there are multiple answers, output any of them.
+
+# For example, given
+
+# A = [12, 28, 46, 32, 50]
+# B = [50, 12, 32, 46, 28]
+# We should return
+# [1, 4, 3, 2, 0]
+# as P[0] = 1 because the 0th element of A appears at B[1], and P[1] = 4 because the 1st element of A appears at B[4], and so on.
+# Note:
+
+# A, B have equal lengths in range [1, 100].
+# A[i], B[i] are integers in range [0, 10^5].
+
+
+class Solution(object):
+    def anagramMappings(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: List[int]
+        """
+        if not A:
+            return []
+        result = [None for _ in range(len(A))]
+        invB = {B[i]:[] for i in range(len(B))}
+        for i in range(len(B)):
+            invB[B[i]].append(i)
+        for i in range(len(A)):
+            result[i] = invB[A[i]].pop()
+        return result
