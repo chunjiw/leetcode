12f9daad7ca308a562418a1f8988eb8ec36cce0f
Chunji Wang <chunjiw@users.noreply.github.com>
2018-07-28 12:12:37 -0700

    Update L253_minMeetingRooms.py

--- L253_minMeetingRooms.py
+++ L253_minMeetingRooms.py
@@ -1,21 +1,21 @@
-# 253. Meeting Rooms II
-DescriptionHintsSubmissionsDiscussSolution
-Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.
+# # 253. Meeting Rooms II
+# DescriptionHintsSubmissionsDiscussSolution
+# Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.
 
-Example 1:
+# Example 1:
 
-Input: [[0, 30],[5, 10],[15, 20]]
-Output: 2
-Example 2:
+# Input: [[0, 30],[5, 10],[15, 20]]
+# Output: 2
+# Example 2:
 
-Input: [[7,10],[2,4]]
-Output: 1
+# Input: [[7,10],[2,4]]
+# Output: 1
 
-# Definition for an interval.
-# class Interval(object):
-#     def __init__(self, s=0, e=0):
-#         self.start = s
-#         self.end = e
+# # Definition for an interval.
+# # class Interval(object):
+# #     def __init__(self, s=0, e=0):
+# #         self.start = s
+# #         self.end = e
 
 
 from heapq import heappush, heappop
@@ -26,8 +26,6 @@ class Solution(object):
         :type intervals: List[Interval]
         :rtype: int
         """
-        if not intervals:
-            return 0
         rooms = []
         intervals.sort(key = lambda x: x.start)
         
