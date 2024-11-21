ffeac474a09a218d4705a2d495cf54db921ecd5f
Emma Lee <emma@nixbase>
2024-11-21 00:28:04 -0800

    Improve 238; python

--- 238 Product of Array Except Self/productExceptSelf.py
+++ 238 Product of Array Except Self/productExceptSelf.py
@@ -1,14 +1,15 @@
+#    a   b   c   d
+#    1   a  ab  abc 
+#   bcd  cd  d   1
+
 class Solution:
   def productExceptSelf(self, nums: List[int]) -> List[int]:
     n = len(nums)
-    incum = list(nums)    # increasing cum prod; incum[n-1] is not used
-    decum = list(nums)    # decreasing cum prod; decum[0] is not used
-    for i in range(1, n-1):
-      incum[i] = incum[i-1] * nums[i]
-      decum[-i-1] = decum[-i] * nums[-i-1]
-    result = list(nums)
-    result[0] = decum[1]
-    result[-1] = incum[-2]
-    for i in range(1, n-1):
-      result[i] = incum[i-1] * decum[i+1]
-    return result
+    incum = [1] * n
+    for i in range(1, n):
+      incum[i] = incum[i-1] * nums[i-1]
+    decum = 1
+    for i in range(n-2, -1, -1):
+      decum *= nums[i+1]
+      incum[i] *= decum
+    return incum
