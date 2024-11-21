8790d2caae5ecf176247b9a7002fb9cf303b97c3
Emma Lee <emma@nixbase>
2024-11-21 00:20:24 -0800

    Improve 238

--- 238 Product of Array Except Self/product_except_self.rs
+++ 238 Product of Array Except Self/product_except_self.rs
@@ -3,16 +3,13 @@ impl Solution {
         let n = nums.len(); // `n` is a runtime-determined value
         let mut incum = vec![1; n]; // Dynamic vector to track prefix products
         let mut decum = vec![1; n]; // Dynamic vector to track suffix products
-        let mut result = vec![1; n];
         for i in 1..n {
             incum[i] = incum[i-1] * nums[i-1];
         }
         for i in (0..n-1).rev() {
             decum[i] = decum[i+1] * nums[i+1];
+            incum[i] *= decum[i]
         }
-        for i in 0..n {
-            result[i] = incum[i] * decum[i];
-        }
-        result
+        incum
     }
 }
