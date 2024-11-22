b25f7ab1815e5bb2d2e30806c4afdce90f210778
Emma Lee <emma@nixbase>
2024-11-22 12:49:00 -0800

    Improve 135: remove recursion

--- 135 Candy/candy.py
+++ 135 Candy/candy.py
@@ -1,20 +1,25 @@
 from typing import List
 
 class Solution:
+
+    def sumcandy(self, rl, fl):
+        return rl * (rl-1) // 2 + fl * (fl-1) // 2 + max(rl, fl)
+
     def candy(self, ratings: List[int]) -> int:
         n = len(ratings)
         if n < 2:
             return n
-        # break the array if neighbors have same ratings
-        for i in range(1, n):
-            if ratings[i - 1] == ratings[i]:
-                return self.candy(ratings[:i]) + self.candy(ratings[i:])
         result = 0
         # loop over ratings
         rl = 0
         fl = 0
         rising = True
         for i in range(1, n):
+            if ratings[i-1] == ratings[i]:
+                result += self.sumcandy(rl, fl)
+                rising = True
+                rl, fl = 0, 0
+                continue
             if rising:
                 if ratings[i-1] < ratings[i]:
                     rl += 1
@@ -25,11 +30,10 @@ class Solution:
                 if ratings[i-1] > ratings[i]:
                     fl += 1
                 else:
+                    result += self.sumcandy(rl, fl)
                     rising = True
-                    result += rl * (rl-1) // 2 + fl * (fl-1) // 2 + max(rl, fl)
-                    rl = 1
-                    fl = 0
-        result += rl * (rl-1) // 2 + fl * (fl-1) // 2 + max(rl, fl)
+                    rl, fl = 1, 0
+        result += self.sumcandy(rl, fl)
         return result + n
 
 
