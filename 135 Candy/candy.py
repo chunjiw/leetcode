645586dec2d3976094416d6db7fb8e7bec162e81
Emma Lee <emma@nixbase>
2024-11-22 14:26:02 -0800

    Improve 135

--- 135 Candy/candy.py
+++ 135 Candy/candy.py
@@ -1,37 +1,27 @@
 from typing import List
 
 class Solution:
-
-    def sumcandy(self, rl, fl):
-        return rl * (rl-1) // 2 + fl * (fl-1) // 2 + max(rl, fl)
-
     def candy(self, ratings: List[int]) -> int:
-        n = len(ratings)
-        result = 0
-        # loop over ratings
-        rl, fl = 0, 0
-        risingBefore = True
-        for i in range(1, n):
-            if ratings[i-1] == ratings[i]:
-                result += self.sumcandy(rl, fl)
-                risingBefore = True
-                rl, fl = 0, 0
-            elif ratings[i-1] < ratings[i]:
-                if risingBefore:
-                    rl += 1
-                else:    # falling turns to rising
-                    result += self.sumcandy(rl, fl)
-                    risingBefore = True
-                    rl, fl = 1, 0
+        if not ratings:
+            return 0
+        prev = ratings[0]
+        up, down, peak, res = 0, 0, 0, 0
+        for curr in ratings[1:]:
+            if prev == curr:
+                up, down, peak = 0, 0, 0
+            elif prev < curr:
+                up += 1
+                peak = up
+                res += up
+                down = 0
             else:
-                if not risingBefore:
-                    fl += 1
-                else:    # risingBefore turns to falling
-                    risingBefore = False
-                    fl = 1
-        result += self.sumcandy(rl, fl)
-        return result + n
-
+                down += 1
+                res += down
+                if down <= peak:
+                    res -= 1
+                up = 0
+            prev = curr
+        return res + len(ratings)
 
 sol = Solution()
 print(sol.candy([1,3,2,2,1]))
