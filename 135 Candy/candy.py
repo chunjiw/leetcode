c03e2cc4affe05835e035e9a3d29ccad9b7e61e7
Emma Lee <emma@nixbase>
2024-11-22 02:54:26 -0800

    Improve 135; python

--- 135 Candy/candy.py
+++ 135 Candy/candy.py
@@ -10,39 +10,34 @@ class Solution:
             if ratings[i - 1] == ratings[i]:
                 return self.candy(ratings[:i]) + self.candy(ratings[i:])
         result = 0
-        # decreasing ratings on the left
-        broken = False
-        for i in range(n - 1):
-            if ratings[i] < ratings[i+1]:
-                result += i * (i+1) // 2
-                broken = True
-                break
-        if not broken:
-            return (n+1) * n // 2
-        # decreasing ratings on the right
-        broken = False
-        for j in range(n - 1):
-            if ratings[-j-2] > ratings[-j-1]:
-                result += j * (j+1) // 2
-                broken = True
-                break
-        if not broken:
-            return (n+1) * n // 2
-        # from here, from i to n-j-1 (include), is minimum to minimum.
-        # there should be other m (>=0) minimums, and m + 1 maximums.
-        mi = [i]
-        ma = []
-        for k in range(i+1, n-j-1):
-            if ratings[k] < min(ratings[k-1], ratings[k+1]):
-                mi.append(k)
-            if ratings[k] > max(ratings[k-1], ratings[k+1]):
-                ma.append(k)
-        mi.append(n-j-1)
-        for k in range(len(ma)):
-            left = ma[k] - mi[k]
-            right = mi[k+1] - ma[k]
-            result += left * (left-1) // 2 + right * (right-1) // 2 + max(left, right)
+        # loop over ratings
+        rl = 0
+        fl = 0
+        if ratings[0] < ratings[1]:
+            rising = True
+            rl = 1
+        else:
+            rising = False
+            fl = 1
+        
+        for i in range(2, n):
+            if rising:
+                if ratings[i-1] < ratings[i]:
+                    rl += 1
+                else:
+                    rising = False
+                    fl = 1
+            else:
+                if ratings[i-1] > ratings[i]:
+                    fl += 1
+                else:
+                    rising = True
+                    result += rl * (rl-1) // 2 + fl * (fl-1) // 2 + max(rl, fl)
+                    rl = 1
+                    fl = 0
+        result += rl * (rl-1) // 2 + fl * (fl-1) // 2 + max(rl, fl)
         return result + n
+
 
 sol = Solution()
 print(sol.candy([1,3,2,2,1]))
