01346a501039d71ad636c54fd5b1cf7a0dc28d3a
Emma Lee <emma@nixbase>
2024-11-23 11:48:33 -0800

    Improve 134

--- 134 Gas Station/can_complete_circuit.rs
+++ 134 Gas Station/can_complete_circuit.rs
@@ -2,24 +2,22 @@ impl Solution {
     pub fn can_complete_circuit(gas: Vec<i32>, cost: Vec<i32>) -> i32 {
         let n = gas.len();
         let mut net = vec![0; n];    // net gas remained after arriving next station
+        let mut prev = 0;
+        let mut mingas = 0;
+        let mut index = 0;
         for i in 0..n {
             net[i] = gas[i] - cost[i];
             if i > 0 {
                 net[i] += net[i-1];
             }
-        }
-        if net[n-1] < 0 {
-            return -1;
-        }
-        let mut prev = 0;
-        let mut mingas = 0;
-        let mut index = 0;
-        for i in 0..n {
             if net[i] >= prev && prev <= mingas {
                 mingas = prev;
                 index = i;
             }
             prev = net[i];
+        }
+        if net[n-1] < 0 {
+            return -1;
         }
         index as i32
     }
