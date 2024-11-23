f1c1c2d37135e0c5ed914f62352973689b66ca8d
Emma Lee <emma@nixbase>
2024-11-23 01:16:31 -0800

    Improve 42

--- 42 Trapping Rain Water/trap2.py
+++ 42 Trapping Rain Water/trap2.py
@@ -1,22 +1,25 @@
-# O(2n), O(2n)
-
 from typing import List
 
 class Solution:
     def trap(self, height: List[int]) -> int:
         n = len(height)
-        leftmax = list(height)
-        rightmax = list(height)
-        lm, rm = 0, 0
-        for i in range(n):
-            leftmax[i], rightmax[-i-1] = lm, rm
-            lm = max(lm, height[i])
-            rm = max(rm, height[-i-1])
-        result = 0
-        for i in range(n):
-            delta = min(leftmax[i], rightmax[i]) - height[i]
-            result += max(0, delta)
-        return result
+        left, right = 0, n-1
+        lmax, rmax = 0, 0
+        water = 0
+        while left <= right:
+            if lmax <= rmax:
+                if lmax >= height[left]:
+                    water += lmax - height[left]
+                else:
+                    lmax = height[left]
+                left += 1
+            else:
+                if rmax >= height[right]:
+                    water += rmax - height[right]
+                else:
+                    rmax = height[right]
+                right -= 1
+        return water
 
 sol = Solution()
 # height = [0,1,0,2,1,0,1,3,2,1,2,1]
