a048af7308e681bd1344030593b35c5dd3b46196
Emma Lee <emma@nixbase>
2024-11-24 12:10:03 -0800

    Improve 3364

--- 3364 Minimum Positive Sum Subarray/minimumSumSubarray.py
+++ 3364 Minimum Positive Sum Subarray/minimumSumSubarray.py
@@ -3,25 +3,21 @@ import sys
 
 class Solution:
     def minimumSumSubarray(self, nums: List[int], l: int, r: int) -> int:
-        cumsum = 0
         n = len(nums)
-        for i in range(n):
-            nums[i] += cumsum
-            cumsum = nums[i]
-        nums.insert(0, 0)
+        i, j = 0, 0
         result = sys.maxsize
         for i in range(n):
-            for size in range(l, r+1):
-                if i + size < n + 1:
-                    s = nums[i + size] - nums[i]
-                    print(i, size, s)
-                    if s > 0 and s < result:
-                        result = s
+            for k in range(l, r+1):
+                if i + k < n + 1:
+                    j = i + k
+                    ls = sum(nums[i:j])
+                    if ls > 0 and ls < result:
+                        result = ls
         if result == sys.maxsize:
             result = -1
         return result
 
 sol = Solution()
-# print(sol.minimumSumSubarray([3, -2, 1, 4], 2, 3))
-# print(sol.minimumSumSubarray([-2, 2, -3, 1], 2, 3))
+print(sol.minimumSumSubarray([3, -2, 1, 4], 2, 3))
+print(sol.minimumSumSubarray([-2, 2, -3, 1], 2, 3))
 print(sol.minimumSumSubarray([1, 2, 3, 4], 2, 4))
