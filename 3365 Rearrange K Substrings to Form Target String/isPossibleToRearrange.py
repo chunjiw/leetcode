b7dd4d1de63a35e49617100b2f58e9f9e6888ab9
Sun Xing Zhe <xing@pop-os>
2024-11-25 15:56:36 -0800

    Improve 3365

--- 3365 Rearrange K Substrings to Form Target String/isPossibleToRearrange.py
+++ 3365 Rearrange K Substrings to Form Target String/isPossibleToRearrange.py
@@ -1,18 +1,16 @@
 class Solution:
     def isPossibleToRearrange(self, s: str, t: str, k: int) -> bool:
-        sl = list(s)
-        tl = list(t)
-        n = len(sl)
+        n = len(s)
         p = n // k
         count = dict()
         for i in range(k):
-            sub = ''.join(sl[p*i : p*i+p])
+            sub = s[p*i : p*i+p]
             if sub not in count:
                 count[sub] = 1
             else:
                 count[sub] += 1
         for i in range(k):
-            sub = ''.join(tl[p*i : p*i+p])
+            sub = t[p*i : p*i+p]
             if sub not in count:
                 return False
             else:
