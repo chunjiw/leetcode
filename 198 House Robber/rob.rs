922776e5bd4ddd2c05c56c8b078198e03314543b
Sun Xing Zhe <xing@pop-os>
2024-11-26 12:44:39 -0800

    Add 198; rust

---
+++ 198 House Robber/rob.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn rob(nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        if n == 0 {
+            return 0;
+        } else if n == 1 {
+            return nums[0];
+        }
+        let mut sol = vec![nums[0]; n];
+        sol[0] = nums[0];
+        sol[1] = nums[0].max(nums[1]);
+        for i in 2..n {
+            sol[i] = sol[i-1].max(sol[i-2] + nums[i]);
+        }
+        sol[n-1]
+    }
+}
