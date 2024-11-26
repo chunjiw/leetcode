7201e2d92368b085e8c678015a41addf40222270
Sun Xing Zhe <xing@pop-os>
2024-11-26 14:53:34 -0800

    Update 116; recursion

--- 116 Populating Next Right Pointers in Each Node/connect_recursion.py
+++ 116 Populating Next Right Pointers in Each Node/connect_recursion.py
@@ -1,56 +1,53 @@
 # 116. Populating Next Right Pointers in Each Node
-# DescriptionHintsSubmissionsDiscussSolution
-# Given a binary tree
+# You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
 
-# struct TreeLinkNode {
-#   TreeLinkNode *left;
-#   TreeLinkNode *right;
-#   TreeLinkNode *next;
+# struct Node {
+#   int val;
+#   Node *left;
+#   Node *right;
+#   Node *next;
 # }
 # Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
 
 # Initially, all next pointers are set to NULL.
 
-# Note:
+ 
 
-# You may only use constant extra space.
-# Recursive approach is fine, implicit stack space does not count as extra space for this problem.
-# You may assume that it is a perfect binary tree (ie, all leaves are at the same level, and every parent has two children).
-# Example:
+# Example 1:
 
-# Given the following perfect binary tree,
 
-#      1
-#    /  \
-#   2    3
-#  / \  / \
-# 4  5  6  7
-# After calling your function, the tree should look like:
+# Input: root = [1,2,3,4,5,6,7]
+# Output: [1,#,2,3,#,4,5,6,7,#]
+# Explanation: Given the above perfect binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+# Example 2:
 
-#      1 -> NULL
-#    /  \
-#   2 -> 3 -> NULL
-#  / \  / \
-# 4->5->6->7 -> NULL
+# Input: root = []
+# Output: []
+ 
+
+# Constraints:
+
+# The number of nodes in the tree is in the range [0, 212 - 1].
+# -1000 <= Node.val <= 1000
 
 
 
-# Definition for binary tree with next pointer.
-# class TreeLinkNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-#         self.next = None
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
 
 class Solution:
-    # @param root, a tree link node
-    # @return nothing
-    # recursively connect
-    def connect(self, root):
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
         if not root:
-            return
+            return root
         self.helper(root.left, root.right)
+        return root
     
     def helper(self, left, right):
         if not left or not right:
