7a6196bd07e3572cff7624a4ca31164ef9079a2e
Sun Xing Zhe <xing@pop-os>
2024-11-27 19:29:35 -0800

    Improve 2290

--- 2290 Minimum Obstacle Removal to Reach Corner/minimumObstacles.py
+++ 2290 Minimum Obstacle Removal to Reach Corner/minimumObstacles.py
@@ -2,6 +2,7 @@ from typing import List
 
 import sys
 from collections import deque
+from heapq import heappush, heappop
 
 class Solution:
     def neighbors(self, r, c, m, n):
@@ -20,14 +21,16 @@ class Solution:
         m, n = len(grid), len(grid[0])
         distances = [[sys.maxsize for _ in range(n)] for _ in range(m)]
         distances[0][0] = 0
-        queue = deque([(0, 0)])
+        queue = [(0, 0, 0)]    # distance, row, col
         while queue:
-            i, j = queue.popleft()
+            dist, i, j = heappop(queue)
+            if i == m - 1 and j == n - 1:
+                return dist
             for (ii, jj) in self.neighbors(i, j, m, n):
                 curr_dist = distances[i][j] + grid[ii][jj]
                 if curr_dist < distances[ii][jj]:
                     distances[ii][jj] = curr_dist
-                    queue.append((ii, jj))
+                    heappush(queue, (curr_dist, ii, jj))
         return distances[m-1][n-1]
 
 sol = Solution()
