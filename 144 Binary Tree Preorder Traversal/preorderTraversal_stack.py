da4acec8d11b3e8bc018435d049e9edb6259c798
Sun Xing Zhe <xing@pop-os>
2024-11-30 14:19:41 -0800

    Add 144; stack

---
+++ 144 Binary Tree Preorder Traversal/preorderTraversal_stack.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result, stack = [], []
+        while True:
+            while root:
+                result.append(root.val)
+                stack.append(root)
+                root = root.left
+            if not stack:
+                return result
+            # gaurantee node at top of stack, node.left is already visited or None
+            root = stack.pop()
+            root = root.right
