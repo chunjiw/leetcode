dc82a46fc2671ac456236758da417b2775062544
Larry Kim <larry@debian3>
2024-12-04 23:55:08 -0800

    Improve 2337

--- 2337 Move Pieces to Obtain a String/main.py
+++ 2337 Move Pieces to Obtain a String/main.py
@@ -1,6 +1,5 @@
 class Solution:
-
-    def segment(self, start, target) -> bool:
+    def canChange(self, start: str, target: str) -> bool:
         i, j = 0, 0
         n = len(target)
         while i < n or j < n:
@@ -18,26 +17,7 @@ class Solution:
             elif i < n or j < n:
                 return False
             i, j = i+1, j+1
-        return True            
-
-    def canChange(self, start: str, target: str) -> bool:
-        if start == target:
-            return True
-        start = list(start)
-        target = list(target)
-        n = len(start)
-        # look for transitions from R to L in target
-        prev = None
-        left = 0
-        for i in range(n):
-            if prev == 'R' and target[i] == 'L':
-                # check segment (left, i - 1)
-                if not self.segment(start[left : i], target[left : i]):
-                    return False
-                left = i
-            if target[i] != '_':
-                prev = target[i]
-        return self.segment(start[left:], target[left:])
+        return True
 
 sol = Solution()
 print(sol.canChange("_L__R__R_", "L______RR"))
