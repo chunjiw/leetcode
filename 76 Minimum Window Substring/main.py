778fdc284bb9a11b84e92aa9dded22ed4ff3781d
Larry Kim <larry@debian3>
2024-12-06 00:41:08 -0800

    Improve 76

--- 76 Minimum Window Substring/main.py
+++ 76 Minimum Window Substring/main.py
@@ -1,48 +1,49 @@
 class Solution:
-
-    def __init__(self):
-        self.map = dict()
-
-    def full(self):
-        return all([curr >= total for curr, total in self.map.values()])
-
     def minWindow(self, s: str, t: str) -> str:
+        map = dict()
         for l in t:
-            _, count = self.map.get(l, [0,0])
-            self.map[l] = [0, count + 1]
+            map[l] = map.get(l, 0) + 1
+        count = len(t)
         n = len(s)
-        min_len = n + 1
+        left, right = -1, -1
         result = ""
-        i = -1
+        min_len = n + 1
+
+        # make sure s[left] is in t
         for k in range(n):
-            if s[k] in self.map:
-                i, j = k, k+1     # [i,j) is in the map; j-i is current length; s[i] in t
-                self.map[s[k]][0] = 1
+            if s[k] in map:
+                left, right = k, k+1     # [left,right) is in the map; right-left is current length; s[left] in t
+                map[s[left]] -= 1
+                count -= 1
                 break
-        if i < 0:
+        if left < 0:
             return result
-        while i < n:
-            if self.full():
-                if j - i < min_len:
-                    min_len = j - i
-                    result = s[i:j]
-                self.map[s[i]][0] -= 1
-                i += 1
-                while i < n and s[i] not in self.map:
-                    i += 1
+
+        # sliding window
+        while left < n:
+            if count == 0:
+                if right - left < min_len:
+                    min_len = right - left
+                    result = s[left:right]
+                map[s[left]] += 1
+                if map[s[left]] > 0:
+                    count += 1
+                left += 1
+                while left < n and s[left] not in map:
+                    left += 1
             else:
-                while j < n and s[j] not in self.map:
-                    j += 1
-                if j < n:
-                    self.map[s[j]][0] += 1
-                    j += 1
+                while right < n and s[right] not in map:
+                    right += 1
+                if right < n:
+                    if map[s[right]] > 0:
+                        count -= 1
+                    map[s[right]] -= 1
+                    right += 1
                 else:
                     break
         return result
 
 sol = Solution()
 print(sol.minWindow("ADOBECODEBANC", "ABC"))
-sol = Solution()
 print(sol.minWindow("a", "a"))
-sol = Solution()
 print(sol.minWindow("b", "a"))
