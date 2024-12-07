ea42ff4229a3411dd5404fae6c74fe998e5c84b9
Larry Kim <larry@debian3>
2024-12-06 16:48:44 -0800

    Improve 56; in place

--- problems/56 Merge Intervals/main.rs
+++ problems/56 Merge Intervals/main.rs
@@ -1,6 +1,27 @@
 struct Solution;
 
 impl Solution {
+
+    pub fn merge_in_place(mut intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        if intervals.is_empty() {
+            return intervals;
+        }
+        intervals.sort_unstable_by(|a, b| a[1].cmp(&b[1]));
+        let n = intervals.len();
+        let mut j = n - 1;    // i: to be merged; j: to be merged to
+        for i in (0..n-1).rev() {
+            if intervals[i][1] < intervals[j][0] {
+                j -= 1;
+                // copy i to j as the new range to be merged to
+                intervals[j][0] = intervals[i][0];
+                intervals[j][1] = intervals[i][1];
+            } else {
+                intervals[j][0] = intervals[j][0].min(intervals[i][0]);
+            }
+        }
+        intervals[j..n].to_vec()
+    }
+
     pub fn merge(mut intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
         let mut result = vec![];
         if intervals.is_empty() {
@@ -25,5 +46,6 @@ impl Solution {
 }
 
 fn main() {
+    println!("{:?}", Solution::merge_in_place(vec![vec![1,3],vec![2,6],vec![8,10],vec![15,18]]));
     println!("{:?}", Solution::merge(vec![vec![1,3],vec![2,6],vec![8,10],vec![15,18]]));
 }
