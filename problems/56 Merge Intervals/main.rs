397d106cc8dde2a7a19df7b22699f486915621ea
Larry Kim <larry@debian3>
2024-12-06 16:03:25 -0800

    Improve 56

--- problems/56 Merge Intervals/main.rs
+++ problems/56 Merge Intervals/main.rs
@@ -2,17 +2,25 @@ struct Solution;
 
 impl Solution {
     pub fn merge(mut intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-        intervals.sort_by(|a, b| a[0].cmp(&b[0]));
-        let mut i = 0;
-        while i + 1 < intervals.len() {
-            if intervals[i][1] >= intervals[i+1][0] {
-                intervals[i][1] = intervals[i][1].max(intervals[i+1][1]);
-                intervals.remove(i + 1);
+        let mut result = vec![];
+        if intervals.is_empty() {
+            return result;
+        }
+        intervals.sort_unstable_by(|a, b| a[0].cmp(&b[0]));
+        let mut current = intervals[0].clone();
+        for (i, range) in intervals.iter().enumerate() {
+            if current[1] < range[0] {
+                result.push(current.clone());
+                current[0] = range[0];
+                current[1] = range[1];
             } else {
-                i += 1;
+                current[1] = current[1].max(range[1]);
+            }
+            if i == intervals.len() - 1 {
+                result.push(current.clone());
             }
         }
-        intervals
+        result
     }
 }
 
