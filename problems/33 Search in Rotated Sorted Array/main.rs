2dc17b22243d9a79c617de8f3676edc6a8000eda
Larry Kim <larry@debian3>
2024-12-07 13:41:56 -0800

    Add 33. Search in Rotated Sorted Array

---
+++ problems/33 Search in Rotated Sorted Array/main.rs
@@ -0,0 +1,31 @@
+struct Solution;
+
+impl Solution {
+    pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+        let (mut i, mut j) = (0, nums.len() - 1);
+        while i < j {
+            let m = i + (j - i) / 2;
+            if nums[m] == target {
+                return m as i32;
+            }
+            if (nums[i] < nums[j] && nums[m] < target)
+            || (i == m)
+            || (nums[j] < nums[i] && nums[i] < nums[m] && nums[m] < target)
+            || (target <= nums[j] && nums[j] < nums[i] && nums[i] < nums[m])
+            || (nums[m] < target && target <= nums[j] && nums[j] < nums[i]) {
+                i = m + 1;
+            } else {
+                j = m;
+            }
+        }
+        if nums[i] == target { i as i32 } else { -1 }
+    }
+}
+
+fn main() {
+    println!("{}", Solution::search(vec![4,5,6,7,0,1,2], 0));
+    println!("{}", Solution::search(vec![4,5,6,7,0,1,2], 1));
+    println!("{}", Solution::search(vec![4,5,6,7,0,1,2], 6));
+    println!("{}", Solution::search(vec![4,5,6,7,0,1,2], 3));
+    println!("{}", Solution::search(vec![1], 0));
+}
