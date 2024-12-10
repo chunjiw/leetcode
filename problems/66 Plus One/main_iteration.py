96e6c0ce632ca52c036c5e8965b230968ea38f7b
Larry Kim <larry@debian3>
2024-12-10 02:58:10 -0800

    Add 66; iteration

---
+++ problems/66 Plus One/main_iteration.py
@@ -0,0 +1,12 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        i = len(digits) - 1
+        while i >= 0 and digits[i] == 9:
+            digits[i] = 0
+            i -= 1
+        if i >= 0:
+            digits[i] += 1
+        else:
+            return [1] + digits
+        return digits
+        
