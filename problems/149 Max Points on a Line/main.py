1938d18046a930d87c7b4e4e3d3b8df15e28ed8a
Larry Kim <larry@debian3>
2024-12-09 18:09:18 -0800

    Improve 149

--- problems/149 Max Points on a Line/main.py
+++ problems/149 Max Points on a Line/main.py
@@ -8,27 +8,18 @@ class Solution:
         x2, y2 = p2[0], p2[1]
         return x0*y1 + x1*y2 + x2*y0 == x2*y1 + x1*y0 + x0*y2
 
-    def getTwo(self, line):
-        it = iter(line)
-        return next(it), next(it)
-
     def maxPoints(self, points: List[List[int]]) -> int:
-        lines = []
         n = len(points)
         if n < 2:
             return n
-        for i, pointi in enumerate(points):
-            for j, pointj in enumerate(points[i+1:]):
-                already_in = False
-                pi = (pointi[0], pointi[1])
-                pj = (pointj[0], pointj[1])
-                for line in lines:
-                    p1, p2 = self.getTwo(line)
-                    if self.isLine(p1, p2, pi) and self.isLine(p1, p2, pj):
-                        already_in = True
-                        line.add(pi)
-                        line.add(pj)
-                        break
-                if not already_in:
-                    lines.append(set([pi, pj]))
-        return max([len(line) for line in lines])
+        result = 0
+        for i, pi in enumerate(points):
+            for j in range(i+1, n):
+                pj = points[j]
+                curr_len = 2
+                for k in range(j+1, n):
+                    pk = points[k]
+                    if self.isLine(pi, pj, pk):
+                        curr_len += 1
+                result = max(result, curr_len)
+        return result
