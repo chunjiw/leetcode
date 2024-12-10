6c2be4448c5e32c8740c9708964de10f1af230e6
Larry Kim <larry@debian3>
2024-12-09 17:40:08 -0800

    Improve 149

--- problems/149 Max Points on a Line/main.py
+++ problems/149 Max Points on a Line/main.py
@@ -2,10 +2,10 @@ from typing import List
 
 class Solution:
 
-    def isLine(self, points: List[List[int]]) -> bool:
-        (x0, y0) = (points[0][0], points[0][1])
-        (x1, y1) = (points[1][0], points[1][1])
-        (x2, y2) = (points[2][0], points[2][1])
+    def isLine(self, p0, p1, p2) -> bool:
+        x0, y0 = p0[0], p0[1]
+        x1, y1 = p1[0], p1[1]
+        x2, y2 = p2[0], p2[1]
         return x0*y1 + x1*y2 + x2*y0 == x2*y1 + x1*y0 + x0*y2
 
     def getTwo(self, line):
@@ -23,8 +23,8 @@ class Solution:
                 pi = (pointi[0], pointi[1])
                 pj = (pointj[0], pointj[1])
                 for line in lines:
-                    point1, point2 = self.getTwo(line)
-                    if self.isLine([point1, point2, pointi]) and self.isLine([point1, point2, pointj]):
+                    p1, p2 = self.getTwo(line)
+                    if self.isLine(p1, p2, pi) and self.isLine(p1, p2, pj):
                         already_in = True
                         line.add(pi)
                         line.add(pj)
