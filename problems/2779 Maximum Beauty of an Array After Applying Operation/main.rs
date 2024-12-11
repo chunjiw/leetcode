cdfa1767fa73bddd39d61a47b131e4e6e5fc2070
chunjiw <chunjiw@hill>
2024-12-10 17:45:17 -0800

    Add 2779; sliding window

--- problems/2779 Maximum Beauty of an Array After Applying Operation/main.rs
+++ problems/2779 Maximum Beauty of an Array After Applying Operation/main.rs
@@ -1,7 +1,25 @@
 struct Solution;
 
+use std::collections::VecDeque;
+
 impl Solution {
     pub fn maximum_beauty(mut nums: Vec<i32>, k: i32) -> i32 {
+        let n = nums.len();
+        if n < 2 { return n as i32; }
+        nums.sort_unstable();
+        let mut deq = VecDeque::from([nums[0]]);
+        let mut beauty = 1;
+        for i in 1..n {
+            deq.push_back(nums[i]);
+            while deq.len() > 0 && *deq.front().unwrap() < nums[i] - 2 * k {
+                deq.pop_front();
+            }
+            beauty = beauty.max(deq.len());
+        }
+        beauty as i32
+    }
+
+    pub fn maximum_beauty_binary_search(mut nums: Vec<i32>, k: i32) -> i32 {
         nums.sort_unstable();
         let mut beauty = 1;
         for (ni, num) in nums.iter().enumerate() {
@@ -24,7 +42,7 @@ impl Solution {
 }
 
 fn main() {
-    println!("{}", Solution::maximum_beauty(vec![4,6,1,2], 2));
+    println!("{}", Solution::maximum_beauty_binary_search(vec![4,6,1,2], 2));
     println!("{}", Solution::maximum_beauty(vec![1,1,1,1], 10));
     println!("{}", Solution::maximum_beauty(vec![52,34], 21));
 }
