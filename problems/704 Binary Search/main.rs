5b408965ee35dc1dc5bcd2e3a0c5a48e6f7d0bbc
Larry Kim <larry@debian3>
2024-12-11 15:30:50 -0800

    Add comment to 704

--- problems/704 Binary Search/main.rs
+++ problems/704 Binary Search/main.rs
@@ -10,12 +10,15 @@ impl Solution {
             } else if nums[m] > target {
                 j = m;
             } else {
+                println!("Target found within loop");
                 return m as i32;
             }
         }
         return if nums[i] == target {
+            println!("Target found outside of loop");
             i as i32
         } else {
+            println!("Target not found but index i is at {i}");
             -1
         }
     }
@@ -27,4 +30,7 @@ fn main() {
     println!("target is at right most position: {}", Solution::search(vec![-1,0,3,5,9,12], 12));
     println!("list has only one element: {}", Solution::search(vec![5], 5));
     println!("list has only two elements: {}", Solution::search(vec![5,6], 6));
+    println!("target does not exist but in the middle: {}", Solution::search(vec![-1,0,3,5,9,12], 4));
+    println!("target does not exist, it is out to the left: {}", Solution::search(vec![-1,0,3,5,9,12], -2));
+    println!("target does not exist, it is out to the right: {}", Solution::search(vec![-1,0,3,5,9,12], 13));
 }
