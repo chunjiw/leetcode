31dd9ef4e1e309cbae9ada7f60aa707425ad1fa3
Larry Kim <larry@debian3>
2024-12-15 12:29:51 -0800

    Improve 190

--- problems/190 Reverse Bits/main.py
+++ problems/190 Reverse Bits/main.py
@@ -1,5 +1,12 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
+        result = 0
+        for i in range(32):
+            result |= ((n & 1) << (31 - i))
+            n >>= 1
+        return result
+
+    def reverseBits2(self, n: int) -> int:
         for i in range(16):
             left = (n >> (31 - i)) & 1
             right = (n >> i) & 1
