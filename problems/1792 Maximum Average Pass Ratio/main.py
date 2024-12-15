ee3adeaa0455a76ff4a6df3eb759506db14b5dc7
Larry Kim <larry@debian3>
2024-12-14 21:48:02 -0800

    Improve 1792

--- problems/1792 Maximum Average Pass Ratio/main.py
+++ problems/1792 Maximum Average Pass Ratio/main.py
@@ -1,21 +1,8 @@
-from heapq import heappop, heappush
-
 class Solution:
-
-    def gain(self, cl):
-        m, n = cl[0], cl[1]
-        return (n-m) / n / (n+1)
-
     def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
-        queue = []
-        for cl in classes:
-            heappush(queue, (-self.gain(cl), cl))
-        print(classes)
-        n = len(classes)
-        while extraStudents > 0:
-            extraStudents -= 1
-            cl = heappop(queue)
-            cl[0] += 1
-            cl[1] += 1
-            heappush(queue, (-self.gain(cl), cl))
-        return sum([cl[0] / cl[1] for cl in classes]) / n
+        queue = [ ((m-n)/n/(n+1), m, n) for m, n in classes ]
+        heapify(queue)
+        for _ in range(extraStudents):
+            _, m, n = heappop(queue)
+            heappush(queue, ((m-n)/(n+1)/(n+2), m+1, n+1))
+        return sum([m / n for _, m, n in queue]) / len(classes)
