02bc78e15b2acbda040808b73d4e01e08e147b78
Larry Kim <larry@debian3>
2024-12-14 18:08:23 -0800

    Add 73 Set Matrix Zero

--- problems/73 Set Matrix Zero/main.rs
+++ problems/73 Set Matrix Zero/main.rs
@@ -1,22 +1,48 @@
 impl Solution {
     pub fn set_zeroes(matrix: &mut Vec<Vec<i32>>) {
-        let mark = matrix.iter().flatten().sum::<i32>() + 1;
-        for i in 0..matrix.len() {
-            for j in 0..matrix[0].len() {
-                if matrix[i][j] != 0 { continue; }
-                for k in 0..matrix.len() {
-                    matrix[k][j] = if matrix[k][j] != 0 { mark } else { 0 };
-                }
-                for k in 0..matrix[0].len() {
-                    matrix[i][k] = if matrix[i][k] != 0 { mark } else { 0 };
+        let (m, n) = (matrix.len(), matrix[0].len());
+        let mut first_row_marked = false;
+        let mut first_col_marked = false;
+        for j in 0..n {
+            if matrix[0][j] == 0 {
+                first_row_marked = true;
+            }
+        }
+        for i in 0..m {
+            if matrix[i][0] == 0 {
+                first_col_marked = true;
+            }
+        }
+        for i in 1..m {
+            for j in 1..n {
+                if matrix[i][j] == 0 { 
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0; 
                 }
             }
         }
-        for i in 0..matrix.len() {
-            for j in 0..matrix[0].len() {
-                if matrix[i][j] == mark {
+        for j in 1..n {
+            if matrix[0][j] == 0 {
+                for i in 1..m {
                     matrix[i][j] = 0;
                 }
+            }
+        }
+        for i in 1..m {
+            if matrix[i][0] == 0 {
+                for j in 1..n {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if first_row_marked {
+            for j in 0..n {
+                matrix[0][j] = 0;
+            }
+        }
+        if first_col_marked {
+            for i in 0..m {
+                matrix[i][0] = 0;
             }
         }
     }
