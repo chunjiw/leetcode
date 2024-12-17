f95b3663a0901ba4726262909ea1dc8a0b7a422f
chunjiw <chunjiw@hill>
2024-12-16 18:09:51 -0800

    Improve 224

--- problems/224 Basic Calculator/main2.py
+++ problems/224 Basic Calculator/main2.py
@@ -4,34 +4,26 @@ class Solution:
         stack = [1]     # keep track of sign of this parenthesis
         num, sign = 0, 1
         result = 0
-        i = 0
-        while i < len(s):
-            if s[i].isdigit():
+        for i, ch in enumerate(s):
+            if ch.isdigit():
                 num *= 10
-                num += int(s[i])
-                i += 1
-                continue
-            result += num * sign * stack[-1]
-            num, sign = 0, 1
-            if s[i:i+2] == '+(':
-                stack.append(stack[-1])
-                i += 1
-            elif s[i:i+2] == '-(':
-                stack.append(-1 * stack[-1])
-                i += 1
-            elif s[i] == ')':
-                stack.pop()
-            elif s[i] == '(':
-                stack.append(stack[-1])
-            elif s[i] == '-':
+                num += int(ch)
+            elif i > 0 and s[i-1].isdigit():
+                result += num * sign * stack[-1]
+                num, sign = 0, 1
+            if ch == '-':
                 sign *= -1
-            i += 1
+            elif ch == '(':
+                stack.append(sign * stack[-1])
+                sign = 1
+            elif ch == ')':
+                stack.pop()
         result += num * sign * stack[-1]
         return result
 
 sol = Solution()
-print(sol.calculate("(9)"))
-# print(sol.calculate("(1+(4+5+2)-3)+(6+8)"))
-# print(sol.calculate("(1+(4+5+2)-3)"))
-# print(sol.calculate("-(6+8)"))
-# print(sol.calculate("-(1+(4+5+2)-3)-(6+8)"))
+print(sol.calculate("-(9)"))
+print(sol.calculate("(1+(4+5+2)-3)+(6+8)"))
+print(sol.calculate("(1+(4+5+2)-3)"))
+print(sol.calculate("-(6+8)"))
+print(sol.calculate("-(1+(4+5+2)-3)-(6+8)"))
