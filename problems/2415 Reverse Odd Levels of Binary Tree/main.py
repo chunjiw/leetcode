3861f5b32938c44ec2f90a52524916d6e502cba5
chunjiw <chunjiw@hill>
2024-12-19 17:04:28 -0800

    Add 2415; BFS and DFS

--- problems/2415 Reverse Odd Levels of Binary Tree/main.py
+++ problems/2415 Reverse Odd Levels of Binary Tree/main.py
@@ -8,7 +8,18 @@
 from collections import deque
 
 class Solution:
+
+    def reverse(self, left, right, level):
+        if level % 2 == 0:
+            left.val, right.val = right.val, left.val
+            self.reverse(left.left, right.right, level + 1)
+            self.reverse(left.right, right.left)
+
     def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        self.reverse(root.left, root.right, 0)
+        return root
+
+    def reverseOddLevels_BFS(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         queue = deque([root])
         level = 0
         while queue:
