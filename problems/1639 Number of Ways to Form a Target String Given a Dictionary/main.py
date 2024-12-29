7dd487ba54457d81e2776f914c025dcb1d071979
Larry Kim <larry@debian3>
2024-12-28 22:47:48 -0800

    Improve 1639

--- problems/1639 Number of Ways to Form a Target String Given a Dictionary/main.py
+++ problems/1639 Number of Ways to Form a Target String Given a Dictionary/main.py
@@ -5,12 +5,14 @@ class Solution:
         m = len(target)
         n = len(words[0])
         modulo = 1000000007
-        dp = [[0] * n for _ in range(m)]
+        dp = [0] * n
         counts = [Counter([word[j] for word in words]) for j in range(n)]
-        dp[-1][-1] = counts[-1][target[-1]] % modulo
+        dp[-1] = counts[-1][target[-1]] % modulo
         for j in range(-2, -n-1, -1):
-            dp[-1][j] = (dp[-1][j+1] + counts[j][target[-1]]) % modulo
+            dp[j] = (dp[j+1] + counts[j][target[-1]]) % modulo
         for i in range(-2, -m-1, -1):
+            ndp = [0] * n
             for j in range(-2, -n-1, -1):
-                dp[i][j] = (dp[i][j+1] + dp[i+1][j+1] * counts[j][target[i]]) % modulo
-        return dp[0][0]
+                ndp[j] = (ndp[j+1] + dp[j+1] * counts[j][target[i]]) % modulo
+            dp = ndp
+        return dp[0]
