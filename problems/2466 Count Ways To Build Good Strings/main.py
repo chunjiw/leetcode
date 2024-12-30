0507fa234bd3febbf70fa36a2fc7d2eee581b295
Larry Kim <larry@debian3>
2024-12-29 17:31:46 -0800

    Add 2466

--- problems/2466 Count Ways To Build Good Strings/main.py
+++ problems/2466 Count Ways To Build Good Strings/main.py
@@ -1,25 +1,18 @@
 class Solution:
-
-    def cal(self, x, zero, one, mem):
-        modulo = 1000000007
-        if x == 0:
-            return 1
-        if x < zero and x < one:
-            return 0
-        if x == zero == one:
-            return 2
-        if min(zero, one) == x:
-            return 1
-        if x in mem:
-            return mem[x]
-        y = self.cal(x - zero, zero, one, mem) + self.cal(x - one, zero, one, mem)
-        mem[x] = y % modulo
-        return y
-
     def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
-        mem = dict()
-        result = 0
+        a, b = zero, one
         modulo = 1000000007
-        for x in range(low, high+1):
-            result = (result + self.cal(x, zero, one, mem)) % modulo
+        result = 0
+        if a > b:
+            a, b = b, a
+        queue = deque([0] * b)
+        queue[b-a] = 1
+        end = a - 1
+        while end < high:
+            queue.append((queue[0] + queue[b-a]) % modulo)
+            queue.popleft()
+            end += 1
+            if end >= low:
+                result = (result + queue[-1]) % modulo
         return result
+        
