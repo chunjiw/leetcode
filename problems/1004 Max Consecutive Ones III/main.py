5d46b64f451d527aa63ac09de8843ad7ed258368
chunjiw <chunjiw@hill>
2025-01-14 16:10:24 -0800

    Add best 1004

--- problems/1004 Max Consecutive Ones III/main.py
+++ problems/1004 Max Consecutive Ones III/main.py
@@ -2,35 +2,19 @@ from typing import List
 
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        n = len(nums)
-        i, j = 0, 0    # longest ones appear in nums[i:j]
-        result = 0
-        maxk = k
-        while j < n:
-            while j < n:
-                if nums[j] == 0:
-                    if k > 0:
-                        k -= 1
-                    else:
-                        break
-                j += 1
-            result = max(result, j - i)
-            while i < j and k == 0:
+        i = 0    # substring of ones is nums[i:j]. After for loop exit, j is effectively len(nums)
+        for j in range(len(nums)):
+            if nums[j] == 0:
+                k -= 1
+            # only move i when we flipped too many zeros
+            if k < 0:
                 if nums[i] == 0:
-                    if k < maxk:
-                        k += 1
-                    else:
-                        break
+                    k += 1
                 i += 1
-            if i == j and maxk == 0:
-                i += 1
-                j += 1
-        return result
+        return len(nums) - i
 
 sol = Solution()
-# print(sol.longestOnes([1,1,1,0,0,0,1,1,1,1,0], 2))
-# print(sol.longestOnes([0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], 3))
-# print(sol.longestOnes([0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], 0))
+print(sol.longestOnes([1,1,1,0,0,0,1,1,1,1,0], 2))
+print(sol.longestOnes([0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], 3))
+print(sol.longestOnes([0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], 0))
 print(sol.longestOnes([0,0,1,1], 1))
-
-
