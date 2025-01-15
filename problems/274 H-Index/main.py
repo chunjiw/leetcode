668e49e555aaeab5efcd44d91620de0be19b73b4
chunjiw <chunjiw@hill>
2025-01-15 14:20:52 -0800

    Add best 274

--- problems/274 H-Index/main.py
+++ problems/274 H-Index/main.py
@@ -1,18 +1,14 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         n = len(citations)
-        # (eventually) delta[i] is number of papers that has at least i+1 citations
-        delta = [0] * (n + 1)    # delta[-1] = 0   
+        # count[i] is number of papers that has i citations
+        # count[n+1] = 0
+        count = [0] * (n + 2)
         for c in citations:
-            if c == 0:
-                continue
-            delta[0] += 1
-            if c <= n-1:
-                delta[c] -= 1
-        result = 0
-        for i in range(n):
-            delta[i] += delta[i-1]
-            if delta[i] >= i+1:
-                result = i+1
-        return result
+            count[min(c, n)] += 1
+        for i in range(n, -1, -1):
+            count[i] += count[i+1]
+            # here count[i] is number of papers that has at least i citations
+            if count[i] >= i:
+                return i
             
