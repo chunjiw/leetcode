7a96e0fa8bfe87de5e65bfb832f6f00c076c43d7
chunjiw <chunjiw@hill>
2025-03-27 12:00:38 -0700

    Fix 3104

--- problems/3104 Find Longest Self-Contained Substring/main.py
+++ problems/3104 Find Longest Self-Contained Substring/main.py
@@ -13,16 +13,17 @@ class Solution:
 
         for i in range(len(s)):
             # substring starts from s[i]
+            if len(s) - i <= result:
+                break
             bag = Counter()
             for j in range(i, len(s)):
-                if j-i+1 <= result:
-                    continue
                 bag[s[j]] += 1
                 if contains(bag, freq) and (i > 0 or j < len(s)-1):
                     result = max(result, j-i+1)
         return result
 
 sol = Solution()
-print(sol.maxSubstringLength("abba"))
-print(sol.maxSubstringLength("abab"))
-print(sol.maxSubstringLength("abacd"))
+# print(sol.maxSubstringLength("abba"))
+# print(sol.maxSubstringLength("abab"))
+# print(sol.maxSubstringLength("abacd"))
+print(sol.maxSubstringLength("off"))
