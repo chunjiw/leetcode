a8f2486315dcc5093b09e0b9ac9e71008b9369f1
chunjiw <chunjiw@hill>
2025-08-05 16:16:16 -0700

    Fix 10

--- problems/1/10 Regular Expression Matching/main.py
+++ problems/1/10 Regular Expression Matching/main.py
@@ -3,7 +3,7 @@ class Solution(object):
         if not p:
             return not s
         
-        first_match = s[0] == p[0] or p[0] == '.'
+        first_match = bool(s) and (s[0] == p[0] or p[0] == '.')
         if len(p) > 1 and p[1] == '*':
             return self.isMatch(s, p[2:]) or (first_match and self.isMatch(s[1:], p))
         return first_match and self.isMatch(s[1:], p[1:])
