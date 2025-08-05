4999f616ce552bfc70c2c5e0c94d308d2b8ad695
chunjiw <chunjiw@hill>
2025-08-05 14:30:55 -0700

    Optimize 815

--- problems/815 Bus Routes/main.py
+++ problems/815 Bus Routes/main.py
@@ -1,13 +1,26 @@
-from collections import deque, defaultdict
+from collections import deque, defaultdict, Counter
 
 class Solution:
     def numBusesToDestination(self, routes: list[list[int]], source: int, target: int) -> int:
         if source == target:
             return 0
+        # stations -> buses
+        bus = defaultdict(set)
+        for busId, route in enumerate(routes):
+            for station in route:
+                bus[station].add(busId)
+        
+        # for key, value in bus:
+        #     if key != source and key != target and len(value) == 1:
+
         graph = defaultdict(set)
         for route in routes:
             for i in range(len(route)):
+                if route[i] != source and route[i] != target and len(bus[route[i]]) == 1:
+                    continue
                 for j in range(i+1, len(route)):
+                    if route[j] != source and route[j] != target and len(bus[route[j]]) == 1:
+                        continue
                     graph[route[i]].add(route[j])
                     graph[route[j]].add(route[i])
         print(graph)
