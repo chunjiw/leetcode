4180de7a4e39f97d5b0fbca2f7ceaa59a4d909f4
chunjiw <chunjiw@hill>
2025-08-05 14:50:14 -0700

    Try 815 again

--- problems/815 Bus Routes/main.py
+++ problems/815 Bus Routes/main.py
@@ -5,38 +5,37 @@ class Solution:
         if source == target:
             return 0
         # stations -> buses
-        bus = defaultdict(set)
+        buses = defaultdict(set)
+        stations = defaultdict(set)
         for busId, route in enumerate(routes):
-            for station in route:
-                bus[station].add(busId)
-        
-        # for key, value in bus:
-        #     if key != source and key != target and len(value) == 1:
+            for stationId in route:
+                stations[stationId].add(busId)
+                buses[busId].add(stationId)
 
-        graph = defaultdict(set)
-        for route in routes:
-            for i in range(len(route)):
-                if route[i] != source and route[i] != target and len(bus[route[i]]) == 1:
-                    continue
-                for j in range(i+1, len(route)):
-                    if route[j] != source and route[j] != target and len(bus[route[j]]) == 1:
-                        continue
-                    graph[route[i]].add(route[j])
-                    graph[route[j]].add(route[i])
-        print(graph)
-        level = deque([source])
-        visited = {source}
+        busGraph = defaultdict(set)
+        nBus = len(routes)
+        for i in range(nBus):
+            for j in range(i+1, nBus):
+                if any(sj in buses[i] for sj in buses[j]):
+                    busGraph[i].add(j)
+                    busGraph[j].add(i)
+        
+        sourceBuses = stations[source]
+        targetBuses = stations[target]
+        
+        level = deque(sourceBuses)
+        visited = set()
         hops = 0
         while level:
             hops += 1
             for _ in range(len(level)):
-                for neighbor in graph[level.popleft()]:
-                    if neighbor in visited:
-                        continue
-                    if neighbor == target:
-                        return hops
-                    level.append(neighbor)
-                    visited.add(neighbor)
+                busId = level.popleft()
+                if busId in targetBuses:
+                    return hops
+                if busId in visited:
+                    continue
+                visited.add(busId)
+                level.extend(busGraph[busId])
         return -1
 
 sol = Solution()
