33b5b02f01c2075cde74842a4e945992fd9d3719
chunjiw <chunjiw@hill>
2025-08-05 16:13:10 -0700

    Add 10

---
+++ problems/1/10 Regular Expression Matching/main.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isMatch(self, s: str, p: str) -> bool:
+        if not p:
+            return not s
+        
+        first_match = s[0] == p[0] or p[0] == '.'
+        if len(p) > 1 and p[1] == '*':
+            return self.isMatch(s, p[2:]) or (first_match and self.isMatch(s[1:], p))
+        return first_match and self.isMatch(s[1:], p[1:])
+
+
+        
+        
+                    
+                    
+                
