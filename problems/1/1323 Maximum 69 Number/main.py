0a4c80fbf9876c357b355f67773ae277b47683b4
chunjiw <chunjiw@hill>
2025-08-15 17:08:29 -0700

    Add 1323

---
+++ problems/1/1323 Maximum 69 Number/main.py
@@ -0,0 +1,21 @@
+class Solution:
+
+    def better(self, num: int) -> int:
+        s = list(str(int))
+        if '6' not in s:
+            return num
+        i = s.index('6')
+        s[i] = '9'
+        return int(''.join(s))
+
+    def maximum69Number(self, num: int) -> int:
+        changed = False
+        result = []
+        for d in str(num):
+            if changed:
+                result.append(d)
+                continue
+            if d == '6':
+                changed = True
+            result.append('9')
+        return int(''.join(result))
