212f41e48e6bea487696623cb8f31d3cbddbb8d4
Chunji Wang <chunji.wang@samsung.com>
2025-09-10 13:30:04 -0700

    Move 293

--- L286_wallsAndGates_main/L286_wallsAndGates_main.py
+++
@@ -1,8 +0,0 @@
-import wallsAndGates
-
-if __name__ == "__main__":
-  sol = wallsAndGates.Solution()
-  rooms = [[2147483647,-1,0,2147483647],[2147483647,2147483647,2147483647,-1],[2147483647,-1,2147483647,-1],[0,-1,2147483647,2147483647]]
-  sol.wallsAndGates(rooms)
-  for room in rooms:
-    print room
